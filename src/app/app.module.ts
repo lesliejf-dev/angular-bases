698cf2e96f3b85d8968781c006240705a2cf216e
Leslie Jara <lesliejf100@gmail.com>
2023-09-08 18:41:14 +0200

    dbz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { CounterModule } from './counter/components/counter.module';
 import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from "./dbz/dbz.module";
 
 import { AppComponent } from './app.component';
 
 
-
 @NgModule({
-  declarations: [
-    AppComponent,
-
-  ],
-  imports: [
-    BrowserModule,
-    CounterModule,
-    HeroesModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        CounterModule,
+        HeroesModule,
+        DbzModule
+    ]
 })
 export class AppModule { }
